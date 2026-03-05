0c734b12421032c3dfd7ae32fc1d8f9a1920f04e
tingchannavong <tingchannavong.gmail.com>
2026-03-05 11:58:15 +0700

    added basic api

--- src/server.js
+++ src/server.js
@@ -6,7 +6,23 @@ const PORT = process.env.PORT;
 
 app.get('/', (re, res) => {
     res.send('Welcome to mini dessert cafe XD');
-})
+});
+
+app.use('/auth', (re, res) => {
+    res.send('auth service');
+});
+
+app.use('/users', (re, res) => {
+    res.send('users service');
+});
+app.use('/desserts', (re, res) => {
+    res.send('desserts service');
+
+});
+
+app.use('/reviews', (re, res) => {
+    res.send('reviews service');
+});
 
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
